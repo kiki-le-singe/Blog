563880b9232c6ce7e59926ef695d02948812b5ad
kiki-le-singe <aalbertini95@gmail.com>
2014-01-31 23:24:55 +0100

    The beginning - Project init : The Kiki's blog

---
+++ install-env.sh
@@ -0,0 +1,11 @@
+################################################################
+# Command shell for install the server development environment #
+################################################################
+
+# @TODO :
+
+# - Install :
+# 	- apache2
+# 	- node : wget http://nodejs.org/dist/v0.10.25/node-v0.10.25-linux-x86.tar.gz
+# 	- Grunt : http://gruntjs.com/getting-started
+# 	- Bower : http://bower.io/
